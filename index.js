ade1cf2dbd828b7b1eb60e2118f848fe17de19f6
Sergey Deshko <axilhik@gmail.com>
2020-07-07 00:07:15 +0300

    Add task 4.4
    add index.js with task 4.4

--- index.js
+++ index.js
@@ -1,58 +1,45 @@
 "use strict";
 
-let day = prompt('Введите день месяца')
+let day = prompt('Введите месяц')
 switch ( day ) {
     case '1':
     case '2':
+    case '12':
+           {
+        let result = 'Зима';
+        alert(result);
+    }
+    break
+
     case '3':
     case '4':
     case '5':
+            {
+        let result = 'Весна';
+        alert(result);
+    }
+    break
+
     case '6':
     case '7':
     case '8':
+            {
+        let result = 'Лето';
+        alert(result);
+    }
+        break
+
     case '9':
     case '10':
-        {
-        let result = 'Первая декада';
-        alert(result);
-    }
-    break
-
     case '11':
-    case '12':
-    case '13':
-    case '14':
-    case '15':
-    case '16':
-    case '17':
-    case '18':
-    case '19':
-    case '20':
-        {
-        let result = 'Вторая декада';
-        alert(result);
-    }
-    break
-
-    case '21':
-    case '22':
-    case '23':
-    case '24':
-    case '25':
-    case '26':
-    case '27':
-    case '28':
-    case '29':
-    case '30':
-    case '31':
-        {
-        let result = 'Третья декада';
+    {
+        let result = 'Осень';
         alert(result);
     }
         break
 
         default: {
-        let result = "в нашем календаре такой даты нет";
+        let result = "в нашем календаре такого месяца нет";
         alert(result);
     }
 
