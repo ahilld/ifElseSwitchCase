ed74d080099a2004d23207181e41659e9fe2d300
Sergey Deshko <axilhik@gmail.com>
2020-07-07 00:02:08 +0300

    Add task 4.2
    add index.js with task 4.2

--- index.js
+++ index.js
@@ -1,32 +1,58 @@
 "use strict";
-let num = prompt('Введите значение переменой')
-switch ( num ) {
-    case '1': {
-        let result = 'Зима';
+
+let day = prompt('Введите день месяца')
+switch ( day ) {
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '10':
+        {
+        let result = 'Первая декада';
         alert(result);
     }
     break
 
-    case '2': {
-        let result = 'Весна';
+    case '11':
+    case '12':
+    case '13':
+    case '14':
+    case '15':
+    case '16':
+    case '17':
+    case '18':
+    case '19':
+    case '20':
+        {
+        let result = 'Вторая декада';
         alert(result);
     }
     break
 
-    case '3': {
-        let result = 'Лето';
+    case '21':
+    case '22':
+    case '23':
+    case '24':
+    case '25':
+    case '26':
+    case '27':
+    case '28':
+    case '29':
+    case '30':
+    case '31':
+        {
+        let result = 'Третья декада';
         alert(result);
     }
         break
 
-    case '4': {
-        let result = 'Осень';
-        alert(result);
-    }
-        break
-
-    default: {
-        let result = "На нашей планете всего 4 времени года";
+        default: {
+        let result = "в нашем календаре такой даты нет";
         alert(result);
     }
 
